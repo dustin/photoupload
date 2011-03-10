ed60ccfcf908c408013d8325572bd36ec320d8cc
Dustin Sallings <dustin@spy.net>
2011-03-10 09:34:00 -0800

    Some minor code formatting fixes.

--- NSData-Base64Extensions.m
+++ NSData-Base64Extensions.m
@@ -32,10 +32,10 @@
 - (NSString *) encodeBase64WithNewlines: (BOOL) encodeWithNewlines;
 {
     // Create a memory buffer which will contain the Base64 encoded string
-    BIO * mem = BIO_new(BIO_s_mem());
+    BIO* mem = BIO_new(BIO_s_mem());
 
     // Push on a Base64 filter so that writing to the buffer encodes the data
-    BIO * b64 = BIO_new(BIO_f_base64());
+    BIO* b64 = BIO_new(BIO_f_base64());
     if (!encodeWithNewlines)
         BIO_set_flags(b64, BIO_FLAGS_BASE64_NO_NL);
     mem = BIO_push(b64, mem);
@@ -45,10 +45,10 @@
     BIO_flush(mem);
 
     // Create a new string from the data in the memory buffer
-    char * base64Pointer;
+    char* base64Pointer;
     long base64Length = BIO_get_mem_data(mem, &base64Pointer);
-    NSString * base64String = [NSString stringWithCString: base64Pointer
-                                                   length: base64Length];
+    NSString* base64String = [NSString stringWithCString: base64Pointer
+                                                length: base64Length];
 
     // Clean up and go home
     BIO_free_all(mem);
